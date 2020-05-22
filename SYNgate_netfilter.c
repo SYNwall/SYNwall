570460ae6c058bb67e9bfbcf0fbebb39def60453
Sorint <mail@example.com>
2020-05-22 17:16:31 +0200

    version 0.3

--- SYNgate_netfilter.c
+++ SYNgate_netfilter.c
@@ -42,7 +42,7 @@
 #include "SYNauth.h"
 
 #define DBGTAG "SYNgate"
-#define VERSION "v0.2"
+#define VERSION "v0.3"
 
 // Max number of destinations managed by the module
 #define MAX_DESTINATIONS   100
